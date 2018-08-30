a658e9b577f5475be1977bfadcf73c243f83e99e
JohanS <joalsj@utu.fi>
2018-08-30 19:58:46 +0300

    run.py lisätty varamuisti

--- run.py
+++ run.py
@@ -3,8 +3,10 @@ import time
 import configure_game as game
 import configure_player as player
 import configure_mails as mail
+import configure
 import Consol
 
+configure.SAVE()
 update = False
 sendMessage = False
 Consol.Message("SERVER ON")
@@ -14,14 +16,17 @@ try:
         timeHouer = int(datetime.datetime.fromtimestamp(time.time()).strftime('%H'))
         if timeHouer == 3 or timeHouer == 5:
             update = game.download_update_liiga(update)
+            configure.SAVE()
             time.sleep(3600)
         if update:
             player.make_updates()
             sendMessage = True
             update = False
+            configure.SAVE()
         if sendMessage and timeHouer == 7:
             mail.send_mail_players_and_admin()
             sendMessage = False
+            configure.SAVE()
             time.sleep(3600)
 except Exception as e:
     Consol.Message("SERVER FAILL: "+e)
