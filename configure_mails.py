ba0e9ccf0397e8489171a6b1e0d440ada34ddb58
Johan Sjövall <joalsj@utu.fi>
2024-09-15 22:00:18 +0300

    update mail system

--- configure_mails.py
+++ configure_mails.py
@@ -9,6 +9,9 @@ from configure import TEAMS_LIST_STRING as teams_list
 from configure import TEAMS_LIST as team_list
 from configure import DB
 from player_data import Player_Data
+from player_data import Team
+from player_data import Player_history_round
+from player_data import History_Team
 
 def send_mail(to, subject, html):    
     try:
@@ -85,9 +88,9 @@ def get_player_guesses_points(PlayerId, GameId):
     data = c.fetchone()
     helpper.disconnectDB()
     return data
-def make_Player_statistics(PlayerGame):
+def make_Player_statistics(player_id, game_id):
     c = helpper.connectDB()
-    c.execute("SELECT Day_ID, "+teams_list+", top4_Correct_Point, Six_Correct_Point, Points FROM PLAYERS_POINTS WHERE Player_ID = ? AND Game_ID = ? ORDER BY Day_ID DESC",(PlayerGame[0],PlayerGame[1]))
+    c.execute("SELECT Day_ID, Points, Six_Correct_Point, top4_Correct_Point, "+teams_list+" FROM PLAYERS_POINTS WHERE Player_ID = ? AND Game_ID = ? ORDER BY Day_ID DESC",(player_id, game_id))
     data = c.fetchall()
     helpper.disconnectDB()
     return(data)
@@ -151,21 +154,118 @@ def getPlayerData():
     data = c.fetchall()
     helpper.disconnectDB()
     for playerData in data:
-        player = Player_Data(playerData[0],playerData[1],playerData[2],playerData[3],playerData[4],playerData[5])
-        player.set_date(league_table[0])
-        pointsData = get_player_guesses_points(player.player_id, player.game_id)
-        playerGuessData = get_player_guesses_list(player.player_id, player.game_id)
-        player.set_total_points(pointsData[0])
-        player.set_six_correct(pointsData[1])
-        player.set_top4_correct(pointsData[2])
-        player.set_teams_data(playerGuessData,league_table[1:],team_list,pointsData[3:])
-        print(player)
+        player = build_player_data(league_table, playerData)
+        message = build_player_message(player)
+        subject = "LIIGAVEIKKAUS PELI "+player.game_name+" "+player.date
+        send_mail(player.player_email,subject,message)
+
+def build_player_data(league_table, playerData):
+    player = Player_Data(playerData[0],playerData[1],playerData[2],playerData[3],playerData[4],playerData[5])
+    player.set_date(league_table[0])
+    pointsData = get_player_guesses_points(player.player_id, player.game_id)
+    playerGuessData = get_player_guesses_list(player.player_id, player.game_id)
+    playerPositionAnd = get_player_Shared_Place_and_Place_and_Day_ID(player.game_name, player.player_id)
+    player.set_shared_place(bool(playerPositionAnd[0]))
+    player.set_position(playerPositionAnd[1])
+    player.set_total_points(pointsData[0])
+    player.set_six_correct(pointsData[1])
+    player.set_top4_correct(pointsData[2])
+    player.set_teams_data(playerGuessData,league_table[1:],team_list,pointsData[3:])
+    return player
+
+def build_player_message(player: Player_Data):
+    player_heder = build_heder(player)
+    palyer_teams = player.teams
+    league_list = sorted(palyer_teams, key=lambda team:team.position)
+    guess_list = sorted(palyer_teams, key=lambda team:team.guess)
+    points_table = player_points_table(team_list, league_list, guess_list, player)
+    history_table = player_history_table(player)
+    message = '''
+<html>
+<body>
+<style>
+table, th, td {
+border: 1px solid black;
+border-collapse: collapse;
+}
+th {
+text-align: left;
+}
+</style>
+<font face="Arial">
+'''+player_heder+'''
+<p></p>
+'''+points_table+'''
+<p></p>
+'''+history_table+'''
+</font>
+</body>
+</html>
+'''
+    return message
+
+def build_heder(player: Player_Data):
+    heder = '<p>Hei <b>'+player.player_first_name+'</b></p>'
+    share_text = '\n</b> pelissa jaetulla sijalla <b>' if player.player_shared_place else '</b> pelissa sijalla <b>'
+    heder += '\n<p>Olet <b>'+player.game_name+share_text+str(player.player_position)+'</b></p>'
+    return heder
+
+def player_points_table(teams_list, league_list, guess_list, player: Player_Data):
+    points_table = '<h3>RUNKOSARJA JA VEIKKAUKSESI: '+str(player.date)+'</h3>'
+    points_table += '\n<table style="width:25%">'
+    points_table += '\n<tr align="left">'
+    points_table += '\n<th>SIJOITUS</th>'
+    points_table += '\n<th>RUNKOSARJA</th>'
+    points_table += '\n<th>VEIKKAUKSESI</th>'
+    points_table += '\n<th>PISTEET</th>'
+    points_table += '\n</tr>'
+    for index in range(len(teams_list)):
+        guess: Team = guess_list[index]
+        position: Team = league_list[index]
+        points_table += '\n<tr>'
+        points_table += '\n<td>'+str(index+1)+'.</td>'
+        points_table += '\n<td>'+position.name+'</td>'
+        points_table += '\n<td>'+guess.name+'</td>'
+        points_table += '\n<td>'+str(guess.points)+'</td>'
+        points_table += '\n</tr>'
+    points_table += '\n</table>'
+    points_table += '\n<p>TOP 4 OIKEIN PISTE: '+str(player.top4_correct)+'</p>'
+    points_table += '\n<p>KUUSI OIKEIN PISTE: '+str(player.six_correct)+'</p>'
+    points_table += '\n<p>KOKONAISPISTEET: <b>'+str(player.total_points)+'</b></p>'
+    return points_table
+
+def player_history_table(player: Player_Data):
+    history_table = '\n<h3>HISTORIASI</h3>'
+    history_table += '\n<table style="width:80%">'
+    history_table += '\n<tr align="left">'
+    history_table += '\n<th>PAIVAMAARA</th>'
+    for index in range(len(team_list)):
+        history_table += '\n<th>'+team_list[index]+'</th>'
+    history_table += '\n<th>KUUSI_OIKEIN</th>'
+    history_table += '\n<th>TOP4_OIKEIN</th>'
+    history_table += '\n<th>KOKONAISPISTEET</th>'
+    history_table += '\n</tr>'
+    for data in make_Player_statistics(player.player_id, player.game_id):
+        player_history = Player_history_round(data)
+        history_table += '\n<tr>'
+        history_table += '\n<td>'+player_history.date+'</td>'
+        for team in player_history.teams:
+           history_table += '\n<td>'+str(team.points)+'</td>'
+        history_table += '\n<td>'+str(player_history.six_correct)+'</td>'
+        history_table += '\n<td>'+str(player_history.top4_correct)+'</td>'
+        history_table += '\n<td>'+str(player_history.total_points)+'</td>'
+        history_table += '\n</tr>'
+    history_table += '\n</table>'
+    return history_table
 
 
-def make_messages(c):
-    league_table = make_liiga_league_table_order(c)
+
+def make_messages():
+    league_table = make_liiga_league_table_order()
+    c = helpper.connectDB()
     c.execute("SELECT MAX(Day_ID) FROM LIIGA_LEAGUE_TABLE")
     day = c.fetchone()[0]
+    helpper.disconnectDB()
     for x in make_players_list(c):
         pName = get_player_name(x[0],c)
         gName = get_game_name(x[1],c)
