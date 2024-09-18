103ef8eced679d32205651cfa3c2d34a5245db6b
Johan Sjövall <joalsj@utu.fi>
2024-09-18 21:13:40 +0300

    update sheed file

--- configure_Sheet.py
+++ configure_Sheet.py
@@ -2,7 +2,8 @@ import datetime
 import time
 import sqlite3
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import helpper
+from google.oauth2.service_account import Credentials
 import Consol
 import configure
 
@@ -21,30 +22,28 @@ class SheetGame:
 
 def Sheet_Table_List():
     gamesSheetList = []
-    conn = sqlite3.connect('Database_liiga_game.db')
-    c = conn.cursor()
+    c = helpper.connectDB()
     c.execute('SELECT Game_ID, Game_staus, Game_history, List_Order FROM SHEET')
+    helpper.disconnectDB()
     for data in c.fetchall():
         gamesSheetList.append(SheetGame(data[0], data[1], data[2], data[3]))
     return gamesSheetList
 
 def Sheet_Run_List():
-    conn = sqlite3.connect('Database_liiga_game.db')
     gamesSheetList = Sheet_Table_List()
     for sheetData in gamesSheetList:
         Sheet_Update(sheetData, conn)
-        Sheet_Player_History(sheetData, conn)
-    conn.close()
+        Sheet_Player_History(sheetData)
 
 
-def Sheet_Update(sheetData: SheetGame, conn: sqlite3.Connection):
-    c = conn.cursor()
-    gameName = getGameName(sheetData, c)
+def Sheet_Update(sheetData: SheetGame):
+    gameName = getGameName(sheetData)
     service = ServiceAccountCredentials.from_json_keyfile_name(configure.CONNECT, configure.SCOPE)
     gc = gspread.authorize(service)
     Consol.Message('SHEET STATUS '+gameName+': START')
     try:
         wks = gc.open(sheetData.staus).sheet1
+        c = helpper.connectDB()
         c.execute('SELECT '+sheetData.getOrder()+', Points FROM '+gameName+' WHERE Day_ID = (SELECT MAX(Day_ID) FROM '+gameName+') ORDER BY '+sheetData.getOrder())
         row = 2
         for x in c.fetchall():
@@ -52,36 +51,44 @@ def Sheet_Update(sheetData: SheetGame, conn: sqlite3.Connection):
             wks.update_cell(row,1,x[0]+' '+x[1])
             wks.update_cell(row,2,x[2])
             row += 1
+        helpper.disconnectDB()
+        c = helpper.connectDB()
         c.execute('SELECT MAX(Day_ID) FROM '+gameName)
         wks.update_cell(1,2,c.fetchone()[0])
+        helpper.disconnectDB()
         Consol.Message('SHEET STATUS '+gameName+': DONE')
     except Exception as e:
-        Consol.Message('SHEET STATUS '+gameName+': ERROR (' + str(e) +')')
+        Consol.ErroMessage('SHEET STATUS '+gameName+': ERROR (' + str(e) +')')
 
-def getGameName(sheetData: SheetGame, c: sqlite3.Cursor):
+def getGameName(sheetData: SheetGame):
+    c = helpper.connectDB()
     c.execute('SELECT Game_Name FROM GAMES WHERE Game_ID = ?',(str(sheetData.id)))
     gameName = c.fetchone()[0]
+    helpper.disconnectDB()
     return gameName
 
-def Sheet_Player_History(sheetData: SheetGame, conn: sqlite3.Connection):
-    c = conn.cursor()
-    gameName = getGameName(sheetData, c)
+def Sheet_Player_History(sheetData: SheetGame):
+    gameName = getGameName(sheetData)
     service = ServiceAccountCredentials.from_json_keyfile_name(configure.CONNECT, configure.SCOPE)
     gc = gspread.authorize(service)
     Consol.Message('START SHEET HISTORY '+gameName+': START')
     try:
         wks = gc.open(sheetData.history).sheet1
+        c = helpper.connectDB()
         c.execute('SELECT MAX(Day_ID) FROM '+gameName)
         Day = c.fetchone()[0]
+        helpper.disconnectDB()
         Line = [Day]
+        c = helpper.connectDB()
         c.execute('SELECT Points FROM '+gameName+' WHERE Day_ID = (SELECT MAX(Day_ID) FROM '+gameName+') ORDER BY Last_Name, First_Name')
         for x in c.fetchall():
             Line += [x[0]]
         #print(Line)
+        helpper.disconnectDB()
         wks.append_row(Line)
         Consol.Message('SHEET HISTORY '+gameName+': DONE')
     except Exception as e:
-        Consol.Message('SHEET HISTORY '+gameName+': ERROR('+str(e)+')')
+        Consol.ErroMessage('SHEET HISTORY '+gameName+': ERROR('+str(e)+')')
 def Sheet_Server_run():
     try:
         Consol.Message('SHEET SERVER: ON')
@@ -94,5 +101,5 @@ def Sheet_Server_run():
                 time.sleep(3600)
                 configure.SAVE()
     except Exception as e:
-        Consol.Message('SHEET SERVER: ERROR ('+str(e)+')')
+        Consol.ErroMessage('SHEET SERVER: ERROR ('+str(e)+')')
         configure.SAVE()
